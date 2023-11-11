bf17f4fddcf74eddb527a46edc329dc7f512afcd
pckerneis <pc.kerneis@gmail.com>
2023-11-11 18:29:05 +0100

    Test with jest

---
+++ src/tokenizer.test.js
@@ -0,0 +1,45 @@
+import {tokenize} from './tokenizer';
+
+test('should tokenize an empty sentence', () => {
+  expect(tokenize('')).toEqual([]);
+});
+
+test('should tokenize a sentence with one word', () => {
+  expect(tokenize('foo')).toEqual([
+    {
+      value: 'foo',
+      line: 1,
+      column: 1,
+    },
+  ]);
+});
+
+test('should tokenize a sentence with two words', () => {
+  expect(tokenize('foo bar')).toEqual([
+    {
+      value: 'foo',
+      line: 1,
+      column: 1,
+    },
+    {
+      value: 'bar',
+      line: 1,
+      column: 5,
+    },
+  ]);
+});
+
+test('should tokenize a sentence with two words and a newline', () => {
+  expect(tokenize('foo\nbar')).toEqual([
+    {
+      value: 'foo',
+      line: 1,
+      column: 1,
+    },
+    {
+      value: 'bar',
+      line: 2,
+      column: 1,
+    },
+  ]);
+});
