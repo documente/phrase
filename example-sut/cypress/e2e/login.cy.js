b2d2479066057cd173a62a216a11acd4d6a870ba
pckerneis <pc.kerneis@gmail.com>
2023-11-12 18:54:23 +0100

    Add cy-runner.js

--- example-sut/cypress/e2e/login.cy.js
+++ example-sut/cypress/e2e/login.cy.js
@@ -1,6 +1,6 @@
-import { buildInstructions } from '../../../lib/src/instruction-builder';
+import {withTree} from '../../../lib/src/cy-runner';
 
-const tree = {
+const test = withTree({
   loginForm: {
     _selector: '.login-form',
     loginField: 'input[type="text"]',
@@ -8,33 +8,16 @@ const tree = {
     confirmButton: 'button[type="submit"]',
   },
   welcomeMessage: 'h1',
-};
+});
 
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/');
 
-    const test = (strings) => {
-      const instructions = buildInstructions(strings[0], tree);
-
-      instructions.actions.forEach(({ target, action, args }) => {
-        if (action === 'click') {
-          return cy.get(target.join(' ')).click();
-        } else if (action === 'type') {
-          cy.get(target.join(' ')).type(args[0].slice(1, -1));
-        }
-      });
-
-      instructions.assertions.forEach(({ target, assertion }) => {
-        if (assertion === 'be visible') {
-          cy.get(target.join(' ')).should('be.visible');
-        }
-      });
-    };
-
     test `when I type "username" on login form login field
-        and I type "password" on login form password field
-        and I click on login form confirm button
-        then welcome message should be visible`;
+          and I type "password" on login form password field
+          and I click on login form confirm button
+          then welcome message should be visible
+          and welcome message should have text "Welcome, username!"`
   });
 })
