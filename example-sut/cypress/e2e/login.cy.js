24a9c7cbb03d540bdd7a95e18258c1adc840a539
pckerneis <pc.kerneis@gmail.com>
2023-11-12 19:21:27 +0100

    Allow parameter interpolation in template string inputs

--- example-sut/cypress/e2e/login.cy.js
+++ example-sut/cypress/e2e/login.cy.js
@@ -10,14 +10,15 @@ const test = withTree({
   welcomeMessage: 'h1',
 });
 
+const baseUrl = 'http://localhost:3000/';
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000/');
-
-    test `when I type "username" on login form login field
-          and I type "password" on password field
-          and I click on login form confirm button
+    test `when I visit "${baseUrl}"
+           and I type "username" on login form login field
+           and I type "password" on password field
+           and I click on login form confirm button
           then welcome message should be visible
-          and welcome message should have text "Welcome, username!"`
+           and welcome message should have text "Welcome, username!"`;
   });
 })
