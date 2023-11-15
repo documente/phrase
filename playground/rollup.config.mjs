4ad3cae7c204f175d5101d84fa0da3c6d29592ea
pckerneis <pc.kerneis@gmail.com>
2023-11-15 12:13:40 +0100

    Migrate playground to vite + typescript and implement run button

--- playground/rollup.config.mjs
+++ playground/rollup.config.mjs
@@ -1,6 +1,6 @@
 import {nodeResolve} from "@rollup/plugin-node-resolve"
 export default {
-  input: "./src/editor.mjs",
+  input: "./src/index.ts",
   output: {
     file: "./public/editor.bundle.js",
     format: "iife"
---
+++ playground/src/index.ts
@@ -0,0 +1,48 @@
+import {EditorView, basicSetup} from "codemirror"
+import {javascript} from "@codemirror/lang-javascript"
+// @ts-ignore
+import {buildInstructions} from "../../lib/src/instruction-builder";
+
+const treeEditor = new EditorView({
+  doc: `{
+  welcomePage: {
+    _selector: '.welcome',
+    greetButton: 'button',
+    welcomeMessage: '.message'
+  }
+}
+`,
+  extensions: [basicSetup, javascript()],
+  parent: document.body.querySelector("#tree-editor")!,
+});
+
+const testEditor = new EditorView({
+  doc: `when I click on welcome page greet button
+then welcome message should be visible
+and it should have text "Hello, World!"`,
+  extensions: [basicSetup, javascript()],
+  parent: document.body.querySelector("#test-editor")!,
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  (window as any)['__buildInstructions'] = buildInstructions;
+
+  document.body.querySelector('#run-button')?.addEventListener('click', () => {
+    const tree = treeEditor.state.doc.toString();
+    const test = testEditor.state.doc.toString();
+    const fullDoc = `try {
+      const instructions = __buildInstructions(\`${test}\`, ${tree});
+      const outputElement = document.querySelector('#output');
+      outputElement.innerText = JSON.stringify(instructions, null, 2);
+      outputElement.classList.remove('error');      
+    } catch(e) {
+      const outputElement = document.querySelector('#output');
+      outputElement.innerText = e.message;
+      outputElement.classList.add('error');
+    }`;
+
+    new Function(fullDoc)();
+  });
+});
+
