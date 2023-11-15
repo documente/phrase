f426ec34f22afcb6634b356d5d04c9ca0f34b42d
pckerneis <pc.kerneis@gmail.com>
2023-11-15 13:54:38 +0100

    Playground: remove obsolete rollup config

--- playground/rollup.config.mjs
+++
@@ -1,9 +0,0 @@
-import {nodeResolve} from "@rollup/plugin-node-resolve"
-export default {
-  input: "./src/index.ts",
-  output: {
-    file: "./public/editor.bundle.js",
-    format: "iife"
-  },
-  plugins: [nodeResolve()]
-}
