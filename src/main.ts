5b6af37f6868eb8c311057882ba2a001699acfd1
pckerneis <pc.kerneis@gmail.com>
2023-12-08 11:01:19 +0100

    Update export list

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,5 @@ export * from './interfaces/statements.interface';
 export * from './interfaces/target-selector.interface';
 export * from './interfaces/token.interface';
 export * from './splitter';
+export * from './normalize-eol';
+export { BuiltinAssertionCode } from './instructions-builder/builtin-assertions';
